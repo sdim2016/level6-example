b68c992cfd89493600ba2f25ce6c12d328ddf8a4
Dmitrii Slepnev <odmsl002@edu.xamk.fi>
2020-04-10 12:55:12 +0300

    Implemented MainActivity functionality

--- app/src/main/java/com/slepnev/numberskotlin/ui/MainActivity.kt
+++ app/src/main/java/com/slepnev/numberskotlin/ui/MainActivity.kt
@@ -5,36 +5,45 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.slepnev.numberskotlin.R
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainActivityViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        initViews()
+        initViewModel()
+    }
+
+    private fun initViews() {
+        fab.setOnClickListener {
+            viewModel.getRandomTrivia() // Get a random number trivia when the fab is clicked
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
+    private fun initViewModel() {
+        // Initialize the MainActivityViewModel
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+
+        // Observe the trivia object
+        viewModel.trivia.observe(this, Observer {
+            tvTrivia.text = it?.text
+        })
+
+        viewModel.error.observe(this, Observer {
+            Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
+        })
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
 }
